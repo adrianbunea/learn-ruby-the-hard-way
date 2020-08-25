398cafb17ec82203d98c500b904dff887843aff2
Adrian Bunea <adrian.bunea28@gmail.com>
2020-08-25 20:55:25 +0300

    Ex11

---
+++ ex11.rb
@@ -0,0 +1,16 @@
+print "How old are you? "
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weigh? "
+weight = gets.chomp()
+
+puts "So. you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "What's your name? "
+name = gets.chomp()
+print "What's one of your hobbies? "
+hobby = gets.chomp()
+
+puts "Ok #{name}, so you like #{hobby}?"
+gets.chomp()
