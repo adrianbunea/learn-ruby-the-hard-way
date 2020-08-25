1aa98af085855633cba637405169b783826ac39f
Adrian Bunea <adrian.bunea28@gmail.com>
2020-08-25 20:43:13 +0300

    Ex3

---
+++ ex3.rb
@@ -0,0 +1,37 @@
+# Writes a sentence
+puts "I will now count my chickens:"
+
+# Writes a sentence, followed by the result of the operation
+puts "Hens", 25 + 30 / 6.0
+# Writes a sentence, followed by the result of the operation
+puts "Roosters", 100 - 25 * 3 % 4
+
+# Writes a sentence
+puts "Now I will count the eggs:"
+
+# Writes the result of the operation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6
+
+# Writes a sentence
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Writes the result of the operation
+puts 3 + 2 < 5 - 7
+
+# Writes a sentence, followed by the result of the operation
+puts "What is 3 + 2?", 3 + 2
+# Writes a sentence, followed by the result of the operation
+puts "What is 5 - 7?", 5 - 7
+
+# Writes a sentence
+puts "Oh, that's why it's false."
+
+# Writes a sentence
+puts "How about some more."
+
+# Writes a sentence, followed by the result of the operation
+puts "Is it greater?", 5 > -2
+# Writes a sentence, followed by the result of the operation
+puts "Is it greater or equal?", 5 >= -2
+# Writes a sentence, followed by the result of the operation
+puts "Is it less or equal?", 5 <= -2
