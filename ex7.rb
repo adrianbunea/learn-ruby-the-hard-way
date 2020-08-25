2c7b09bd95a5e8a196c34f2aadec1c37dcec5b86
Adrian Bunea <adrian.bunea28@gmail.com>
2020-08-25 20:44:10 +0300

    Ex7

---
+++ ex7.rb
@@ -0,0 +1,33 @@
+# puts a string
+puts "Mary had a little lamb."
+# puts a formated string
+puts "Its fleece was white as %s." % 'snow'
+# puts a string
+puts "And everywhere that Mary went."
+# puts a dot 10 times
+puts "." * 10 # what'd that do?
+
+# creates 12 variables, each containing a letter
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# notice how we are using print instead of puts here. change it to puts
+# and see what happens.
+# prints the first 6 created letters
+print end1 + end2 + end3 + end4 + end5 + end6
+# prints the other 6 letters, next to the first 6 letters
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# this just is polite use of the terminal, try removing it
+# puts an empy line and ends goes on the next line
+puts
