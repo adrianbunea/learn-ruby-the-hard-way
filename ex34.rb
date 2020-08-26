0d5048cc0a0a72530b4623ecc5b73a915060e813
adrianbunea <adrian_bunea28@yahoo.com>
2020-08-26 14:35:38 +0300

    Ex34

---
+++ ex34.rb
@@ -0,0 +1,9 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts animals[1] # The animal at 1 is the 2nd animal and is a python
+puts animals[2] # The 3rd animal is at 2 and is a peacock
+puts animals[0] # The 1st animal is at 0 and is a bear
+puts animals[3] # The animal at 3 is the 4th animal and is a kangaroo
+puts animals[4] # The 5th animal is at 4 and is a whale
+puts animals[5] # The 6th animal is at 5 and is a platypus
+puts animals[4] # The animal at 4 is the 5th animal and is a whale
